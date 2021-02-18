b054c23510f7c1b0aedd20762706328f9e0971b5
Glenn Hinks <ghinks@yahoo.com>
2021-02-18 11:02:36 -0500

    feat: basic plugin & test
    setup the basics for the plugins

--- src/monitor/index.test.ts
+++ src/monitor/index.test.ts
@@ -1,7 +1,10 @@
 import tap from "tap";
 import myfunction from "./index";
 
-tap.test("basics", (t) => {
-  t.equal(myfunction("abc"), "abc");
-  t.end();
+tap.test("Describe block", (t) => {
+  t.plan(1);
+  t.test("basics", (t) => {
+    t.equal(myfunction("abc"), "abc");
+    t.end();
+  });
 });
